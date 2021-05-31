218ec3228e69990c1b689729beb05b0884f5560c
krishlala <71400038+krishlala@users.noreply.github.com>
2021-05-31 12:17:37 -0400

    Update main.js

--- main.js
+++ main.js
@@ -1,61 +1,65 @@
 var SpeechRecognition = window.webkitSpeechRecognition;
-
+  
 var recognition = new SpeechRecognition();
+
+var Textbox = document.getElementById("textbox"); 
 
 function start()
 {
-    document.getElementById("textbox").innerHTML = "";
+    Textbox.innerHTML = ""; 
     recognition.start();
-}
+} 
+ 
+recognition.onresult = function(event) {
 
-recognition.onresult = function run (event) {
+ console.log(event); 
 
-    console.log(event);
+var Content = event.results[0][0].transcript;
 
-    var Content = event.results[0][0].transcript;
+    Textbox.innerHTML = Content;
     console.log(Content);
-    if(Content ==  "take my selfie")
-    {
-        console.log("Taking selfie ---");
+      if(Content =="take my selfie")
+      {
+        console.log("taking selfie --- ");
         speak();
-    }
+      }
 }
 
-    document.getElementById("textbox").innerHTML = Content;
-}
 
-function speak() {
+function speak(){
     var synth = window.speechSynthesis;
-    
-    speak_data = "Taking your selfie in 5 seconds";
-    
+
+    speak_data = "Taking you Selfie in 5 seconds";
+
     var utterThis = new SpeechSynthesisUtterance(speak_data);
-    
+
     synth.speak(utterThis);
+
     Webcam.attach(camera);
-    
+
     setTimeout(function()
-    {
-        take_snapshot();
+    { 
+        take_selfie(); 
         save();
     }, 5000);
 }
 
-Webcam.set({
-    width: 360,
-    height: 250,
-    image_format: 'png',
-    png_quality: 90
-});
+ 
 camera = document.getElementById("camera");
+Webcam.set({
+    width:360,
+    height:250,
+    image_format : 'jpeg',
+    jpeg_quality:90
+});
 
-
-function take_snapshot()
+function take_selfie()
 {
     Webcam.snap(function(data_uri) {
-        document.getElementById("result").innerHTML = '<img id="selfie_image" src="'+data_uri+'">';
-});
+        document.getElementById("result").innerHTML = '<img id="selfie_image" src="'+data_uri+'"/>';
+    });
 }
+
 
 function save()
 {
